89a6e844ef92cb282f204976d1216af039ad46ce
oompaloompa77 <paula.mariana2019.pmom@gmail.com>
2025-05-15 17:35:27 -0300

    criação do arquivo carro.py

---
+++ carro.py
@@ -0,0 +1,12 @@
+#pedindo para digitar o nome do carro
+carro = input('Digite o nome de um carro: ')
+#pedidndo o valor do carrop
+valor_carro = float(input('Digite o valor do carro: '))
+#pedindo o consumo de combustivel 
+consumo_de_combustivel = float(input('Digite a quantidadr de combustivel que o veiculo consome: '))
+
+#organizando a impressão no terminal
+print('---------------------------------------------------')
+print('O modelo do carro é:, carro ')
+print(f'O valor do carro que é: {valor_carro}')
+print(f'O consumo de combustivel é {consumo_de_combustivel}')
